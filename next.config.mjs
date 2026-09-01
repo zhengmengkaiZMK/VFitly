82c696f630e1c48f6fdfcfef49b279036d0cad8f
zhengmengkaiZMK <zhengmengkai@example.com>
2026-09-01 11:13:02 +0800

    Allow R2 asset domain for images

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
+      {
+        protocol: "https",
+        hostname: "assets.vfitly.com",
+      },
     ],
   },
   pageExtensions: ["ts", "tsx", "mdx"],
